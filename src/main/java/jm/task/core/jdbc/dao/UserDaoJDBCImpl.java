a7b6c129294fdd982321b529760ff1eeaac403ac
Andrew30310 <donskih.1999@bk.ru>
2023-06-09 15:13:08 +0200

    решение

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,89 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private static final String NEW_USER = "INSERT INTO users.users_table (name, lastName, age) VALUES(?,?,?)";
+    private static final String REMOVED_USER = "DELETE FROM users.users_table where ID = ?";
+
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
+        try(Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("CREATE TABLE `users`.`users_table` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT, \n " +
+                    "  `name` VARCHAR(45) NOT NULL,\n" +
+                    "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                    "  `age` INT NOT NULL,\n" +
+                    "  PRIMARY KEY (`id`));");
+            System.out.println("Таблица создана");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Таблица уже существует");
+        }
     }
 
     public void dropUsersTable() {
-
+        try(Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("DROP TABLE `users`.`users_table`;");
+            System.out.println("Таблица удалена");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Указанной таблицы не сущетсвует");
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(NEW_USER)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.execute();
+            System.out.printf("User с именем - %s добавлен в базу данных\n", name);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void removeUserById(long id) {
-
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(REMOVED_USER)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+            System.out.println("User удалён");
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        try(Statement statement = Util.getConnection().createStatement()) {
+            List<User> usersList = new ArrayList<>();
+            ResultSet users = statement.executeQuery("SELECT * from users.users_table");
+            while (users.next()) {
+                User user = new User(users.getString(2), users.getString(3), users.getByte(4));
+                user.setId(users.getLong(1));
+                usersList.add(user);
+            }
+            return usersList;
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void cleanUsersTable() {
-
+        try(Statement statement = Util.getConnection().createStatement()) {
+            statement.execute("TRUNCATE users.users_table");
+            System.out.println("Таблица очищена");
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import javax.xml.stream.FactoryConfigurationError;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String username = "root";
+    private static final String password = "root";
+    private static final String connectionUrl = "jdbc:mysql://localhost:3306/mysql";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Connection connection;
+        Class.forName(driver);
+        return connection = DriverManager.getConnection(connectionUrl, username, password);
+
+    }
 }
