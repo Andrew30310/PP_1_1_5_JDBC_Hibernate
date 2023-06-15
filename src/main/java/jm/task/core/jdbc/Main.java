ba8161336e9679fae34f9f0be62bbbd150a711f7
Andrew30310 <donskih.1999@bk.ru>
2023-06-15 12:48:42 +0200

    решение Hibernate 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,13 +6,14 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserService service = new UserServiceImpl();
-        service.createUsersTable();
-        service.saveUser("Иван", "Иванов", (byte) 54);
-        service.saveUser("Петр", "Петров", (byte) 30);
-        service.saveUser("Сергей", "Сергеев", (byte) 15);
-        service.saveUser("John", "Doe", (byte) 25);
-        System.out.println(service.getAllUsers());
-        service.cleanUsersTable();
-        service.dropUsersTable();
+        //service.createUsersTable();
+        //service.saveUser("Иван", "Иванов", (byte) 54);
+        //service.saveUser("Петр", "Петров", (byte) 30);
+        //service.saveUser("Сергей", "Сергеев", (byte) 15);
+        //service.saveUser("John", "Doe", (byte) 25);
+        //System.out.println(service.getAllUsers());
+        //service.removeUserById(3);
+        //service.cleanUsersTable();
+        //service.dropUsersTable();
     }
 }
